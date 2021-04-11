2bee51b85d30212aac16479d44ae54d767ca55c7
JSischo <jsischo@uwalumni.com>
2021-04-10 21:34:24 -0500

    .R file with Suspension Coil script added.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,8 @@ mpg_results <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_results) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_results)) #generate summary statistics
+
+# Import and Read Suspension Coil .CSV
+Susp_coil_results <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+total_summary <- Susp_coil_results %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+lot_summary <- Susp_coil_results %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
