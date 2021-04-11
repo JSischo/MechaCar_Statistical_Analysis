afa6aa33370651270717b429f987057caf68537b
JSischo <jsischo@uwalumni.com>
2021-04-10 22:36:21 -0500

    T-Test script added to .R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,15 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 Susp_coil_results <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
 total_summary <- Susp_coil_results %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
 lot_summary <- Susp_coil_results %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table with multiple columns
+
+#T-test comparing sample(all lots) to pop.
+t.test((Susp_coil_results$PSI),mu=1500)
+#Subset T-test
+lot1 <- subset(Susp_coil_results, Susp_coil_results$Manufacturing_Lot == "Lot1")
+t.test((lot1$PSI), mu = 1500)
+
+lot2 <- subset(Susp_coil_results, Susp_coil_results$Manufacturing_Lot == "Lot2")
+t.test((lot2$PSI), mu = 1500)
+
+lot3 <- subset(Susp_coil_results, Susp_coil_results$Manufacturing_Lot == "Lot3")
+t.test((lot3$PSI), mu = 1500)
